48e8eea82a5de08e986c4067803cde146f4273b8
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-03-11 08:32:50 +0700

    feat: float example

--- main.c
+++ main.c
@@ -2,12 +2,17 @@
 #include <stdio.h>
 
 LinkedList(int);
+LinkedList(float);
 
 int main() {
+  list_float_t lf = CreateList_float();
   list_int_t ll = CreateList_int();
-  for (int i = 0; i < 10; i++) {
+  for (int i = -5; i < 6; i++) {
     ll.LPush(&ll, i);
+    lf.LPush(&lf, (float)i / 10);
   }
+
+  printf("Int\n\n");
   node_int_t *current = ll.head;
   while (current) {
     printf("%d\n", current->obj);
@@ -22,6 +27,23 @@ int main() {
   printf("\n");
   for (int i = 0; i < ll.count; i++) {
     printf("%d : %d\n", i, ll.At(&ll, i)->obj);
+  }
+
+  printf("\nFloat\n\n");
+  node_float_t *current_f = lf.head;
+  while (current_f) {
+    printf("%f\n", current_f->obj);
+    current_f = current_f->r;
+  }
+  printf("\n");
+  current_f = lf.tail;
+  while (current_f) {
+    printf("%f\n", current_f->obj);
+    current_f = current_f->l;
+  }
+  printf("\n");
+  for (int i = 0; i < lf.count; i++) {
+    printf("%d : %f\n", i, lf.At(&lf, i)->obj);
   }
 
   return 0;
