61bede6ed4288c4ddb5ae03343ea116824939b6d
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-03-11 09:03:08 +0700

    feat: example using custom struct

--- main.c
+++ main.c
@@ -4,12 +4,20 @@
 LinkedList(int);
 LinkedList(float);
 
+typedef struct {
+  float x, y;
+} vector2;
+
+LinkedList(vector2);
+
 int main() {
   list_float_t lf = CreateList_float();
   list_int_t ll = CreateList_int();
+  list_vector2_t lv = CreateList_vector2();
   for (int i = -5; i < 6; i++) {
     ll.LPush(&ll, i);
     lf.LPush(&lf, (float)i / 10);
+    lv.LPush(&lv, (vector2){i, (float)i / 10});
   }
 
   printf("Int\n\n");
@@ -44,6 +52,23 @@ int main() {
   printf("\n");
   for (int i = 0; i < lf.count; i++) {
     printf("%d : %f\n", i, lf.At(&lf, i)->obj);
+  }
+
+  printf("\nVector2\n\n");
+  node_vector2_t *current_v = lv.head;
+  while (current_v) {
+    printf("%f %f\n", current_v->obj.x, current_v->obj.y);
+    current_v = current_v->r;
+  }
+  printf("\n");
+  current_v = lv.tail;
+  while (current_v) {
+    printf("%f %f\n", current_v->obj.x, current_v->obj.y);
+    current_v = current_v->l;
+  }
+  printf("\n");
+  for (int i = 0; i < lv.count; i++) {
+    printf("%d : %f %f\n", i, lv.At(&lv, i)->obj.x, lv.At(&lv, i)->obj.y);
   }
 
   return 0;
